fb31ddf2e3f014b78c004fb5ce265c6a9884c1c2
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-04-12 16:59:19 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,7 +14,9 @@ var firebaseConfig = {
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
     //Start code
-
+  console.log(Room_names)
+  row="<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+  document.getElementById("output").innerHTML+=row;
     //End code
     });});}
 getData();
@@ -29,3 +31,9 @@ function  addRoom(){
       Hobby:"Gaming"
   });
 }
+
+function redirectToRoomName(name){
+  console.log(name);
+  localStorage.setItem("room_name",name);
+  window.location="kwitter_page.html";
+  }
